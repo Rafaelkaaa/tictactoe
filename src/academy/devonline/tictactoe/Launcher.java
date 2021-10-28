afa586d1b6c8d67e35fbee969c05d51d927e2f63
Rafaelkaaa <proskurina.vlada@gmail.com>
2021-10-28 19:14:11 +0300

    Fix  files

--- src/academy/devonline/tictactoe/Launcher.java
+++ src/academy/devonline/tictactoe/Launcher.java
@@ -5,7 +5,11 @@ import academy.devonline.tictactoe.ccomponent.*;
 public class Launcher {
     public static void main(String[] args) {
         final Game game = new Game(
-                new DataPrinter(), new ComputerMove(), new UserMove(), new WinnerVerifier(), new DrawVerifier()
+                new DataPrinter(),
+                new ComputerMove(),
+                new UserMove(),
+                new WinnerVerifier(),
+                new DrawVerifier()
         );
         game.play();
     }
